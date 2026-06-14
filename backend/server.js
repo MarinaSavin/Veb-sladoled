343ed63051871f11f06bc07834075f1d54acfb3a
savinmarinaa@gmail.com <you@example.com>
2026-06-14 21:11:57 +0200

    Prvo komitovanje backend-a

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(port, () =>
+    console.log(`Server is running on port ${port}`)
+);
